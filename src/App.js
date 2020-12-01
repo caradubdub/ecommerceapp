dc8e61bf09f69845260662a4d32d245abea8751b
Cara Dibdin <admin@MacBook-Pro.local>
2020-11-30 20:24:07 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
-import AddProduct from '.components/AddProduct';
-import Cart from '.components/Cart';
-import Login from '.components/Login';
-import ProductList from '.components/ProductList';
+import AddProduct from './components/AddProduct';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import ProductList from './components/ProductList';
 
 import Context from "./Context";
+
 
 export default class App extends Component {
   constructor(props) {
@@ -18,7 +21,54 @@ export default class App extends Component {
     };
     this.routerRef = React.createRef();
   }
+  async componentDidMount() {
+    let user = localStorage.getItem("user");
+    let cart = localStorage.getItem("cart");
 
+    const products = await axios.get('http://localhost:3001/products');
+    user = user ? JSON.parse(user) : null;
+    cart = cart ? JSON.parse(cart) : {};
+    this.setState({ user, products: products.data, cart });
+  }
+  login = async (email, password) => {
+    const res = await axios.post(
+      'http://localhost:3001/login',
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+
+    if (res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  }
+
+  addToCart = cartItem => {
+    let cart = this.state.cart;
+  }
   render() {
     return (
       <Context.Provider
@@ -34,19 +84,22 @@ export default class App extends Component {
       >
         <Router ref={this.routerRef}>
           <div className="App">
-            <nav className="navbar container"
+            <nav
+              className="navbar container"
               role="navigation"
-              aria-label="main navigation">
+              aria-label="main navigation"
+            >
               <div className="navbar-brand">
-                <b className="navbar-item is-size-4">ecommerce</b>
-                <label role="button"
+                <b className="navbar-item is-size-4 ">Natural Skin Care</b>
+                <label
+                  role="button"
                   class="navbar-burger burger"
                   aria-label="menu"
                   aria-expanded="false"
                   data-target="navbarBasicExample"
                   onClick={e => {
                     e.preventDefault();
-                    this.setState({ showMenu: !this.state.showMenu })
+                    this.setState({ showMenu: !this.state.showMenu });
                   }}
                 >
                   <span aria-hidden="true"></span>
@@ -64,20 +117,23 @@ export default class App extends Component {
                     Add Product
                   </Link>
                 )}
-
                 <Link to="/cart" className="navbar-item">
                   Cart
-                  <span className="tag is-primary"
+                  <span
+                    className="tag is-primary"
                     style={{ marginLeft: "5px" }}
                   >
-                    {Object.keys(this.state.cart).length}</span>
+                    {Object.keys(this.state.cart).length}
+                  </span>
                 </Link>
-                {!this.state.user ? (<Link to="/login" className="navbar-item">
-                  login
-                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
                 ) : (
                     <Link to="/" onClick={this.logout} className="navbar-item">
-                      Logout</Link>
+                      Logout
+                    </Link>
                   )}
               </div>
             </nav>
@@ -85,11 +141,12 @@ export default class App extends Component {
               <Route exact path="/" component={ProductList} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
               <Route exact path="/products" component={ProductList} />
             </Switch>
           </div>
         </Router>
       </Context.Provider>
-    )
+    );
   }
 }
